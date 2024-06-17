7eb79399f77e280c5b340243da0b593135c3281a
Annika Kaul <annika-kristin.kaul1@informatik.hs-fulda.de>
2024-06-17 09:25:35 +0200

    added Image Saver

--- test-run.sh
+++ test-run.sh
@@ -9,3 +9,10 @@ ros2 run image_view image_view image:=/image_raw
 # or:
 source /opt/ros/humble/setup.bash
 ros2 run camera_calibration cameracalibrator --size 9x6 --square 0.053 --ros-args -r image:=/image_raw -r camera:=/camera
+
+
+
+# neu,nach 2. in neuem Terminal: -> Take Image, make it to txt file with pixels; make png out of txt file again
+colcon build
+ros2 run my_image_saver image_saver
+python my_image_saver/image_creater.py
